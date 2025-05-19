cc897f28b35dd12d9bbf2fa074d4124b1145f47b
Pradeep Maheepala <hello@eartisan.co.uk>
2025-05-19 22:15:12 +0100

    Added button location

--- floating-whatsapp.php
+++ floating-whatsapp.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Floating WhatsApp Button
 Plugin URI: https://www.eartisan.co.uk
-Description: Adds a floating WhatsApp button on the right side of the page, allowing the admin to add a WhatsApp number and custom message through the settings. Bsed on the Floating WhatsApp plugin by Yoki Rahmada.
-Version: 1.1
+Description: Adds a floating WhatsApp button on the page, allowing the admin to add a WhatsApp number, custom message, and choose position through the settings. Based on the Floating WhatsApp plugin by Yoki Rahmada.
+Version: 1.2
 Author: Pradeep Maheepala
 Author URI: https://www.eartisan.co.uk
 License: GPL2
@@ -40,6 +40,15 @@ function fwa_settings_page() {
                     <th scope="row">Custom WhatsApp Message</th>
                     <td><input type="text" name="fwa_whatsapp_message" value="<?php echo esc_attr(get_option('fwa_whatsapp_message')); ?>" style="width: 250px;" /></td>
                 </tr>
+                <tr valign="top">
+                    <th scope="row">Button Position</th>
+                    <td>
+                        <select name="fwa_button_position" style="width: 250px;">
+                            <option value="right" <?php selected(get_option('fwa_button_position', 'right'), 'right'); ?>>Bottom Right</option>
+                            <option value="left" <?php selected(get_option('fwa_button_position', 'right'), 'left'); ?>>Bottom Left</option>
+                        </select>
+                    </td>
+                </tr>
             </table>
             <?php submit_button(); ?>
         </form>
@@ -51,6 +60,7 @@ function fwa_settings_page() {
 function fwa_register_settings() {
     register_setting('fwa_settings_group', 'fwa_whatsapp_number');  
     register_setting('fwa_settings_group', 'fwa_whatsapp_message');
+    register_setting('fwa_settings_group', 'fwa_button_position');
 }
 add_action('admin_init', 'fwa_register_settings');
 
@@ -59,13 +69,16 @@ function fwa_display_whatsapp_button() {
     
     $whatsapp_number = get_option('fwa_whatsapp_number');
     $whatsapp_message = get_option('fwa_whatsapp_message');
+    $button_position = get_option('fwa_button_position', 'right'); // Default to right if not set
 
     if ($whatsapp_number) {
         
         $encoded_message = urlencode($whatsapp_message);
 
+        // Add position class to the button
+        $position_class = ($button_position === 'left') ? 'fwa-position-left' : 'fwa-position-right';
         
-        echo '<a href="https://wa.me/' . esc_attr($whatsapp_number) . '?text=' . $encoded_message . '" target="_blank" class="fwa-floating-btn">
+        echo '<a href="https://wa.me/' . esc_attr($whatsapp_number) . '?text=' . $encoded_message . '" target="_blank" class="fwa-floating-btn ' . $position_class . '">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp" />
               </a>';
         
@@ -74,13 +87,18 @@ function fwa_display_whatsapp_button() {
             .fwa-floating-btn {
                 position: fixed;
                 bottom: 30px;
-                right: 30px;
-                background-color: #25D366;
                 border-radius: 50%;
                 padding: 15px;
+                background-color: #25D366;
                 box-shadow: 0 6px 15px rgba(0, 0, 0, 0.2);
                 z-index: 9999;
                 transition: all 0.3s ease;
+            }
+            .fwa-position-right {
+                right: 30px;
+            }
+            .fwa-position-left {
+                left: 30px;
             }
             .fwa-floating-btn img {
                 width: 30px;
